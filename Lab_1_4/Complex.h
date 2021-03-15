b16e74fc41466a4b27ebeade84da75ff1cd0819e
MarkStadnytskyi <72573640+MarkStadnytskyi@users.noreply.github.com>
2021-03-15 10:55:25 +0200

    Add files via upload

---
+++ Lab_1_4/Complex.h
@@ -0,0 +1,31 @@
+#pragma once
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Complex
+{
+private:
+	double re, im;
+
+public:
+	double GetRe() const { return re; }
+	double GetIm() const { return im; }
+	void SetRe(double value) { re = value; }
+	void SetIm(double value) { im = value; }
+
+	void Init(double, double);
+	void Read(const char*);
+	void Display();
+
+	string toString() const;
+
+	friend Complex Add(const Complex& a, const Complex& b);
+	friend Complex Sub(const Complex& a, const Complex& b);
+	friend Complex Mul(const Complex& a, const Complex& b);
+
+};
+
+
---
+++ Lab_1_4/Lab_1_4.cpp
@@ -0,0 +1,30 @@
+﻿#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif 
+
+#include <iostream>
+#include "Complex.h"
+
+using namespace std;
+
+int main()
+{
+	Complex z;
+	z.Read("z1");
+	cout << "Display z1";
+	z.Display();
+	cout << "z1.toString() : " << z.toString() << endl;
+
+	Complex z2;
+	z2.Read("z2");
+	cout << "Display z2" << endl;
+	z2.Display();
+	cout << "z2.toString() : " << z2.toString() << endl;
+	cout << "Add(z1, z2) = " << Add(z, z2).toString() << endl;
+	cout << "Sub(z1, z2) = " << Sub(z, z2).toString() << endl;
+	cout << "Mul(z1, z2) = " << Mul(z, z2).toString() << endl;
+
+	cin.get();
+
+	return 0;
+}
